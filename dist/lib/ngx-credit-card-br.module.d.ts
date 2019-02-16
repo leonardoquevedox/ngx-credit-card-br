d30f583404a4c26bb6779ac028b76f5b98c4fdc8
Leonardo Quevedo <pac.leo@hotmail.com>
2019-02-15 23:21:59 -0200

    refact(bundle): Improves the way the module is bundled.

--- dist/lib/ngx-credit-card-br.module.d.ts
+++ dist/lib/ngx-credit-card-br.module.d.ts
@@ -1,8 +1,3 @@
-/**
- * @license MIT
- * @author Leonardo Quevedo
- * @description Component module.
- */
 import { NgxCreditCardBrComponent } from './ngx-credit-card-br.component';
 export declare const Component: (typeof NgxCreditCardBrComponent)[];
 export declare class NgxCreditCardBrModule {
